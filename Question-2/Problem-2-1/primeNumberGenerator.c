a05de936b14b55777d0a42d7004fc724a0fbd10a
PIXLwastaken <janueljmarquez@gmail.com>
2025-10-20 20:13:42 -0400

    idek bro

--- Question-2/Problem-2-1/primeNumberGenerator.c
+++ Question-2/Problem-2-1/primeNumberGenerator.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
 int main(){
-    
+    // int num, i;
+    // int isPrime[];
+
+    // for (i = 2; i <= num; ){
+    //     isPrime[i] = true; // Assume all numbers are prime initially
+    //     i++;
+    // }
 }
