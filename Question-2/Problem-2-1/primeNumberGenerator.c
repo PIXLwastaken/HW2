e6944b3996f436380d87f91a02ddd669c24c4267
PIXLwastaken <janueljmarquez@gmail.com>
2025-11-03 20:12:35 -0500

    Refactor prime number generator: remove debug print statements and add input validation

--- Question-2/Problem-2-1/primeNumberGenerator.c
+++ Question-2/Problem-2-1/primeNumberGenerator.c
@@ -16,17 +16,17 @@ Problem Description:
 
 void sieveOfEratosthenes(int n){
 
-    printf("Step 1: Create a boolean array and init all entries as true\n"); //debug line
+    // printf("Step 1: Create a boolean array and init all entries as true\n"); //debug line
 
     int isPrime[n+1]; //declare an empty array of size n+1 to accomodate all numbers from 0 to n
     isPrime[0] = isPrime[1] = 0; //init index 0 and 1 as false since those arent primes
 
     for (int i = 2; i <= n; i++){
         isPrime[i] = 1; //init all array entries from 2 to n as true
-        printf("%d | %d\n", i, isPrime[i]); //debug line
+        // printf("%d | %d\n", i, isPrime[i]); //debug line
     } 
 
-    printf("\nStep 2: Mark multiples of each prime as composite\n"); //debug line
+    // printf("\nStep 2: Mark multiples of each prime as composite\n"); //debug line
     
     double sqrtN = sqrt(n); //declare and init this outside of for-loop so it isnt calculated every loop
     for (int p = 2; p <= sqrtN; p++){
@@ -34,12 +34,12 @@ void sieveOfEratosthenes(int n){
             //Mark all multiples of p as not prime
             for (int i = p * p; i <= n; i += p){
                 isPrime[i] = 0;
-                printf("%d | %d\n", i, isPrime[i]); //debug line
+                // printf("%d | %d\n", i, isPrime[i]); //debug line
             }
         }
     }
 
-    printf("\nStep 3: Collect all numbers that remain marked as prime\n"); //debug line
+    // printf("\nStep 3: Collect all numbers that remain marked as prime\n"); //debug line
 
     //declare empty array of size n and primeCount to count how many primes are found
     int primes[n], primeCount = 0; 
@@ -47,19 +47,41 @@ void sieveOfEratosthenes(int n){
     for (int i = 2; i <= n; i++){
         if (isPrime[i] == 1){ //if a number is prime
             primes[primeCount] = i; //add it to the primes array
-            printf("%d | %d\n", primes[primeCount], primeCount); //debug line
+            // printf("%d | %d\n", primes[primeCount], primeCount+1); //debug line
             primeCount++; //increase the count of primes found
         }
     }
 
-
+    //print primes in a formatted table
+    for (int i = 0; i < primeCount; i++) {
+        if (i % 10 == 0) {
+            printf("---------------------------------------------------------------------------------\n|");
+        }
+        printf(" %5d |", primes[i]);
+        if ((i + 1) % 10 == 0 || i == primeCount - 1) {
+            printf("\n");
+        }
+    }
+    printf("---------------------------------------------------------------------------------\n");
+    //print total number of primes found
+    printf("Number of primes found: %d\n", primeCount);
 }
 
 int main(){
-    int n; 
+    int n;
 
-    printf("Enter number to look for primes from 1 to n:\n");
-    scanf("%d", &n);
+    printf("Enter a number to look for primes from 1 to n:\n");
+    //input validation
+    while(scanf("%d", &n) != 1){
+        printf("Invalid Input. Enter an integer to look for primes from 1 to n:\n");
+        int c;
+        while((c = getchar()) != '\n' && c != EOF);
+    }
 
+    clock_t start = clock();
     sieveOfEratosthenes(n);
+    clock_t end = clock();
+
+    double delta = (double) (end - start) / CLOCKS_PER_SEC;
+    printf("Time elapsed: %lf\n", delta);
 }
